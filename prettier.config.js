46b49caa1570d0af1044102a56f8d17768a5f052
Eugene Maltsev <hellpirat1@gmail.com>
2020-04-26 10:17:36 +0700

    git init

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "plugins": ["prettier-plugin-svelte"],
+  "svelteSortOrder": "scripts-markup-styles",
+  "svelteStrictMode": true,
+  "svelteBracketNewLine": true,
+  "svelteAllowShorthand": false,
+  "printWidth": 120
+}
---
+++ src/TawkToChatWidget.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from 'svelte'
+
+  /**
+   *  Direct Chat Link from tawk.to dashboard
+   *  https://tawk.to/chat/{tawkToPropertyId}/{tawkToChatId}
+   * /
+
+  /**
+   * Property ID
+   * @type {string}
+   */
+  export let tawkToPropertyId = ''
+
+  /**
+   * Chat id
+   * @type {string}
+   */
+  export let tawkToChatId = 'default'
+
+  onMount(() => {
+    // Widget code from dashboard
+    const Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date()
+    ;(function() {
+      var s1 = document.createElement('script'),
+        s0 = document.getElementsByTagName('script')[0]
+      s1.async = true
+      s1.src = `https://embed.tawk.to/${tawkToPropertyId}/${tawkToChatId}`
+      s1.charset = 'UTF-8'
+      s1.setAttribute('crossorigin', '*')
+      s0.parentNode.insertBefore(s1, s0)
+    })()
+  })
+</script>
